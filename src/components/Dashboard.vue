dff57ffbbaac2a6ade461ed4905b9dd34839316a
Adam Syarif Hidayatullah <adamsyarif219@gmail.com>
2021-08-19 17:52:14 +0700

    update view

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,65 +3,65 @@
 
     <!--  Sidebar  -->
     <div class="col-fixed custom-sidebar">
-      <div class="w-full mt-4 mb-8 text-center">
+      <div class="w-full mt-4 mb-6 text-center">
         <img src="../assets/logo_web.png" class="custom-logo"/>
       </div>
-      <div class="grid align-items-center mx-4 text-gray-600 pointer custom-sidebar-menu-active shadow-2">
+      <div class="grid align-items-center mx-4 text-gray-600 pointer custom-sidebar-menu custom-sidebar-menu-active shadow-2">
         <div class="col text-center">
-          <i class="pi pi-th-large text-3xl"></i>
+          <i class="pi pi-th-large text-xl custom-icon"></i>
         </div>
         <div class="col-fixed custom-text-icon">
-          <p class="text-xl my-0">Dashboard</p>
+          <p class="my-0">Dashboard</p>
         </div>
       </div>
       <Divider class="mb-4"/>
-      <div class="grid align-items-center mx-4 mb-5 text-gray-600 cursor-pointer">
+      <div class="grid align-items-center mx-4 mb-3 text-gray-600 custom-sidebar-menu cursor-pointer">
         <div class="col text-center">
-          <i class="pi pi-user text-3xl"></i>
+          <i class="pi pi-user text-xl custom-icon"></i>
         </div>
         <div class="col-fixed custom-text-icon">
-          <p class="text-xl my-0">Candidates</p>
+          <p class="my-0">Candidates</p>
         </div>
       </div>
-      <div class="grid align-items-center mx-4 mb-5 text-gray-600 cursor-pointer">
+      <div class="grid align-items-center mx-4 mb-3 text-gray-600 custom-sidebar-menu cursor-pointer">
         <div class="col text-center">
-          <i class="pi pi-id-card text-3xl"></i>
+          <i class="pi pi-id-card text-xl custom-icon"></i>
         </div>
         <div class="col-fixed custom-text-icon">
-          <p class="text-xl my-0">Employees</p>
+          <p class="my-0">Employees</p>
         </div>
       </div>
-      <div class="grid align-items-center mx-4 mb-5 text-gray-600 cursor-pointer">
+      <div class="grid align-items-center mx-4 mb-3 text-gray-600 custom-sidebar-menu cursor-pointer">
         <div class="col text-center">
-          <i class="pi pi-calendar-times text-3xl"></i>
+          <i class="pi pi-calendar-times text-xl custom-icon"></i>
         </div>
         <div class="col-fixed custom-text-icon">
-          <p class="text-xl my-0">Standby</p>
+          <p class="my-0">Standby</p>
         </div>
       </div>
-      <div class="grid align-items-center mx-4 mb-5 text-gray-600 cursor-pointer">
+      <div class="grid align-items-center mx-4 mb-3 text-gray-600 custom-sidebar-menu cursor-pointer">
         <div class="col text-center">
-          <i class="pi pi-chart-line text-3xl"></i>
+          <i class="pi pi-chart-line text-xl custom-icon"></i>
         </div>
         <div class="col-fixed custom-text-icon">
-          <p class="text-xl my-0">Reports</p>
+          <p class="my-0">Reports</p>
         </div>
       </div>
       <Divider class="mb-4"/>
-      <div class="grid align-items-center mx-4 mb-5 text-gray-600 cursor-pointer">
+      <div class="grid align-items-center mx-4 mb-3 text-gray-600 custom-sidebar-menu cursor-pointer">
         <div class="col text-center">
-          <i class="pi pi-cog text-3xl"></i>
+          <i class="pi pi-cog text-xl custom-icon"></i>
         </div>
         <div class="col-fixed custom-text-icon">
-          <p class="text-xl my-0">Settings</p>
+          <p class="my-0">Settings</p>
         </div>
       </div>
-      <div class="grid align-items-center mx-4 mb-5 text-gray-600 cursor-pointer">
-        <div class="col text-center">
-          <i class="pi pi-sign-out text-3xl"></i>
+      <div class="grid align-items-center mx-4 mb-3 text-gray-600 custom-sidebar-menu cursor-pointer">
+        <div class="col text-center custom-icon">
+          <i class="pi pi-sign-out text-xl"></i>
         </div>
         <div class="col-fixed custom-text-icon">
-          <p class="text-xl my-0">Sign Out</p>
+          <p class="my-0">Sign Out</p>
         </div>
       </div>
     </div>
@@ -106,18 +106,41 @@ export default {
   //border: 1px solid;
 }
 
-@media only screen and (max-width: 1700px) {
-  .custom-text-icon {
-    display: none !important;
+@media only screen and (min-height: 950px){
+  .custom-sidebar {
+    width: 400px !important;
   }
 
+  .custom-navbar {
+    height: 100px !important;
+  }
 
   .custom-logo {
-    width: 150px !important;
+    width: 120px !important;
+  }
+
+  .custom-text-icon {
+    font-size: 20px !important;
+  }
+
+  .custom-sidebar-menu {
+    margin-bottom: 35px !important;
+  }
+
+  .custom-icon {
+    font-size: 25px !important;
+  }
+
+  .custom-logo {
+    width: 180px !important;
   }
 }
 
-@media only screen and (max-width: 1300px) {
+@media only screen and (max-width: 900px) {
+  .custom-sidebar {
+    width: 150px !important;
+  }
+
   .custom-text-icon {
     display: none !important;
   }
@@ -127,25 +150,23 @@ export default {
   }
 }
 
-@media only screen and (max-width: 900px) {
+@media only screen and (max-width: 650px) {
   .custom-sidebar {
     display: none !important;
   }
 
   .custom-navbar-button {
-    width: 20%;
+    width: 40px !important;
     display: block !important;
   }
-}
 
-@media only screen and (max-width: 375px) {
   .custom-user-name {
-    font-size: 15px;
+    font-size: 13px !important;
   }
 }
 
 .custom-logo {
-  width: 180px;
+  width: 150px;
 }
 
 .custom-admin {
@@ -162,19 +183,18 @@ export default {
 
 .custom-navbar {
   background-color: #ffffff;
-  height: 10%;
+  height: 70px;
   border-bottom: 3px solid grey;
 }
 
 .custom-sidebar {
-  width: 15%;
+  width: 300px;
   background-color: #ffffff;
   border-right: 1px solid grey;
 }
 
 .custom-content {
   background-color: #eff5f7;
-  height: 90%
 }
 
 .custom-text-icon {
